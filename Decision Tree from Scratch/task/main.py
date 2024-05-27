548b5a1846e6e8b755bc290b496889dc90929a2e
radosan <radoslaw.sanetra@gmail.com>
2024-05-27 19:48:22 +0200

    added node splitting function

--- Decision Tree from Scratch/task/main.py
+++ Decision Tree from Scratch/task/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def gini(d: np.ndarray) -> float:
@@ -9,9 +10,21 @@ def weighted_gini(d1: np.ndarray, d2: np.ndarray) -> float:
     return round((len(d1) * gini(d1) + len(d2) * gini(d2)) / (len(d1) + len(d2)), 5)
 
 
+def split(x: pd.DataFrame, y: pd.Series) -> tuple:
+    result = None
+    for feat in x.columns:
+        for val in x[feat].unique():
+            left, right = y[x[feat] == val], y[x[feat] != val]
+            gini_ind = weighted_gini(left, right)
+            if not result or gini_ind < result[0]:
+                result = (gini_ind, feat, val, left.index.tolist(), right.index.tolist())
+    return result
+
+
 def main():
-    data, split1, split2 = (np.array([int(x) for x in input().split()]) for _ in range(3))
-    print(f'{gini(data):.2f} {weighted_gini(split1, split2):.2f}')
+    df = pd.read_csv(input(), index_col=0)
+    data, target = df.drop(columns='Survived'), df['Survived']
+    print(*split(data, target))
 
 
 if __name__ == '__main__':
